eee8a2381c4facc3b02b6141df72cb9ff081c4d5
udit <udit>
2025-01-17 18:38:23 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Menu from './pages/menu';
+import Menu from './pages/Menu';
 import Header from './components/header';
 import Home from './pages/home';
 import Contact from './pages/contact';
